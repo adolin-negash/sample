f2c14123205f2d57d87c76a1e4041add0ec83405
negash <anegash@globalconsult.org>
2021-05-24 17:28:08 +0300

    change Application

--- src/main/java/adolin/sample/Application.java
+++ src/main/java/adolin/sample/Application.java
@@ -4,9 +4,9 @@ import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 
@@ -22,16 +22,15 @@ public class Application {
     @Autowired
     private Environment environment;
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     /**
      * Точка входа в приложение.
      *
      * @param args command line args.
      */
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+            .web(WebApplicationType.SERVLET)
+            .run(args);
     }
 
     /**
